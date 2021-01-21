494204d32172683184b18498b4c016be9417b4a6
Olli <olli.juhola@careeria.fi>
2021-01-21 11:37:52 +0200

    Projekti alustettu

---
+++ Harkat/OlioOhjelmointiWPFSovellukset/Harjoitus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Harjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
