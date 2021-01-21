9eca2b8af704eae1367ba2c598d121fb1a9b3e0d
Olli <olli.juhola@careeria.fi>
2021-01-21 11:57:22 +0200

    Harjoitus 1 Commit

--- Harkat/OlioOhjelmointiWPFSovellukset/Harjoitus/Program.cs
+++ Harkat/OlioOhjelmointiWPFSovellukset/Harjoitus/Program.cs
@@ -6,7 +6,33 @@ namespace Harjoitus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Olio Ajoneuvo -luokasta
+            Ajoneuvo auto = new Ajoneuvo();
+
+            // Asetetaan oliolle "auto" arvot
+            auto.Nimi = "Toyota";
+            auto.Nopeus = 160;
+            auto.Renkaat = 4;
+
+            // Käytetään "auto" olion toimintoja
+            auto.TulostaData();
+
+            // Noudetaan auton tiedot ToString() metodilla
+            string autonTiedot = auto.ToString();
+            Console.WriteLine(autonTiedot);
+
+            // Tyhjä rivi konsoliin
+            Console.WriteLine();
+
+            // Luodaan toinen olio luokasta Ajoneuvo
+
+            Ajoneuvo mopo = new Ajoneuvo();
+            mopo.Nimi = "Skootteri";
+            mopo.Nopeus = 55;
+            mopo.Renkaat = 2;
+
+            mopo.TulostaData();
+            Console.Write(mopo.ToString());
         }
     }
 }
